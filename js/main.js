f7ad2e2218a446d40cbbf3427f92e2541a8e3855
Kevin Salcedo <salcedokevin249@gmail.com>
2026-09-24 01:43:49 -0300

    listo para entregar1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-console.log("Hola mundo");
-alert("Hola mundo");
+const nombre = prompt("¿Cuál es tu nombre?"); 
+console.log("Hola, " + nombre + "!");
+
+const año = parseInt(prompt("¿En que año naciste?")); 
+console.log("Naciste en el año " + año + ".");
+
+let ciudad = prompt("¿En qué ciudad vivís?"); 
+console.log("Vivís en " + ciudad + ".");
+
+
+
+const anioActual = 2026;
+const calculoEdad = anioActual - año;
+
+const mensaje = "Hola, " + nombre + "! Tenés " + calculoEdad + " años y vivís en " + ciudad + ".";
+console.log(mensaje);
+alert(mensaje);
